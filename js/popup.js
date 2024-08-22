a4e8887a065acaaa79ea84c1d5def597fabdff0b
JustKemForFun <haocong728@gmail.com>
2024-08-22 23:06:46 +0700

    chore: bump to 1.0.2

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,15 @@
-chrome.runtime.onMessage.addListener(function (request) {
-  if (request.action === "IMEIValue") {
-    document.getElementById("imei").value = request.imei;
-    document.getElementById("imei").parentElement.classList.remove("is-disabled");
-  } else if (request.action === "CookiesValue") {
-    document.getElementById("cookies").value = request.cookies;
-    document.getElementById("cookies").parentElement.classList.remove("is-disabled");
-  } else if (request.action === "UserAgent") {
-    document.getElementById("user-agent").value = request.useragent;
-    document.getElementById("user-agent").parentElement.classList.remove("is-disabled");
-  }
+// window.addEventListener('load', function() { /* my code */ });
+window.addEventListener("load", function () {
+  chrome.runtime.onMessage.addListener(function (request) {
+    if (request.action === "IMEIValue") {
+      document.getElementById("imei").value = request.imei;
+      document.getElementById("imei").parentElement.classList.remove("is-disabled");
+    } else if (request.action === "CookiesValue") {
+      document.getElementById("cookies").value = request.cookies;
+      document.getElementById("cookies").parentElement.classList.remove("is-disabled");
+    } else if (request.action === "UserAgent") {
+      document.getElementById("user-agent").value = request.useragent;
+      document.getElementById("user-agent").parentElement.classList.remove("is-disabled");
+    }
+  });
 });
