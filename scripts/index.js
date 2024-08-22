644d8f1114fc5d1642ba458f254ff84eb08f9c23
JustKemForFun <haocong728@gmail.com>
2024-08-22 23:28:23 +0700

    chore: bump to 1.0.4

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import { checkUpdate } from './helpers/checkUpdate.js';
 
 (async function () {
   try {
-    await checkUpdate(); // Thêm 'await' để đảm bảo hàm 'checkUpdate' hoàn thành trước khi tiếp tục
+    await checkUpdate();
   } catch (e) {
     console.error("Error calling checkUpdate:", e);
   }
