f118edef12021c5ab613710fe5dd1c3e862337e7
JustKemForFun <haocong728@gmail.com>
2025-09-17 16:15:14 +0700

    chore: remove checkUpdate and update version to 1.1.4 in manifest

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-import { checkUpdate } from './helpers/checkUpdate.js';
-
-(async function () {
-  try {
-    await checkUpdate();
-  } catch (e) {
-    console.error("Error calling checkUpdate:", e);
-  }
-})();
